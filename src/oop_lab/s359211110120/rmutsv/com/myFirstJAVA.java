e0f715906487969a5b13224117dad907a7488b93
Nootak <oinoo.takzii@gmail.com>
2017-11-17 11:19:09 +0700

    Testing edit code

--- src/oop_lab/s359211110120/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110120/rmutsv/com/myFirstJAVA.java
@@ -4,8 +4,7 @@ public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello World,Im'TAK");
         System.out.println("Insormation System");
-        System.out.println("Faculty of Management Technology");
-        System.out.println("RMUTSV");
+        
     }//main
 
 } //class
