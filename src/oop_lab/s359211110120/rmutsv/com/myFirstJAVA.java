0e9feef816dffa28848c3fef6731721d66d37b84
Nootak <oinoo.takzii@gmail.com>
2017-11-17 11:17:37 +0700

    SuccessFull Information data

--- src/oop_lab/s359211110120/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110120/rmutsv/com/myFirstJAVA.java
@@ -2,7 +2,11 @@ package oop_lab.s359211110120.rmutsv.com;
 
 public class myFirstJAVA {
     public static void main(String[] args) {
-        System.out.println("Hello World,ImTAK");
+        System.out.println("Hello World,Im'TAK");
+        System.out.println("Insormation System");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("RMUTSV");
     }//main
 
 } //class
+
