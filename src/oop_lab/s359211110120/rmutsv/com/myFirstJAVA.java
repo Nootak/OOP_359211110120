bc8ebc07a485d61484630a3ada1c1a58d619f15c
Nootak <oinoo.takzii@gmail.com>
2017-11-17 11:29:04 +0700

    Testing edit code

--- src/oop_lab/s359211110120/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110120/rmutsv/com/myFirstJAVA.java
@@ -4,7 +4,6 @@ public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello World,Im'TAK");
         System.out.println("Insormation System");
-        
     }//main
 
 } //class
