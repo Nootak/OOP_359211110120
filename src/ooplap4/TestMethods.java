925614c846dcd731719a59145a79dfa7e3ce3d82
Nootak <oinoo.takzii@gmail.com>
2017-12-20 16:38:03 +0700

    TestSwitchCase

--- src/ooplap4/TestMethods.java
+++ src/ooplap4/TestMethods.java
@@ -3,9 +3,15 @@ package ooplap4;
 public class TestMethods {
     //global variabal
     public static int number=10;
-    public static void main(String[] args) {
-        System.out.println();
 
+//Methods
+// type 2
+        public static void B (int x) {
+            System.out.println("Hello B"+x);
+        }//A
+        public static void main (String[]args) {
+            System.out.println("Hello Main");
+            A();
 
     }//main
 }//class
