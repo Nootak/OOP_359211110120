ff3ef21c4d7b920f29cb960db254f927ff965a5f
Nootak <oinoo.takzii@gmail.com>
2017-12-20 16:30:59 +0700

    TestForLoop

--- src/ooplap4/TestMethods.java
+++ src/ooplap4/TestMethods.java
@@ -1,8 +1,11 @@
 package ooplap4;
 
 public class TestMethods {
+    //global variabal
+    public static int number=10;
     public static void main(String[] args) {
         System.out.println();
+
 
     }//main
 }//class
